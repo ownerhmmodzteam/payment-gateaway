47ebf9a2ce2b096fb1128bd2e28b86bd88389177
Owner HM MODZ TEAM <ownerhmmodzvip@gmail.com>
2026-04-19 10:48:53 +0700

    Create generate-qris.js

---
+++ api/generate-qris.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+
+    const { amount } = req.body;
+    const apiKey = 'MP-hmmodz_store-1776566983';
+    const targetUrl = `https://api.mustika.com/v1/generate?api_key=${apiKey}&amount=${amount}`;
+
+    try {
+        const response = await fetch(targetUrl);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
